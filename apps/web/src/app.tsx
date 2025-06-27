474ff367cc264f3bd9152facf77462504f18a6b5
nqdd <datnq1210@gmail.com>
2025-06-27 23:55:01 +0700

    feat: restructure app layout with sidebar integration
    Move sidebar-related components and logic from main.tsx to app.tsx for better component organization. Add transparent background style for shadcn components.

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,12 +1,23 @@
+import { SidebarProvider, SidebarTrigger, Label } from '@repo/ui';
+import { AppSidebar } from './components/app-sidebar';
 import { MarkdownEditor } from './components/markdown-editor';
 
 function App() {
   return (
-    <div className="flex flex-col gap-4 w-full h-screen p-2">
-      <div className="flex flex-1 border-1 rounded-md">
-        <MarkdownEditor />
+    <SidebarProvider>
+      <AppSidebar />
+      <div className="flex flex-col flex-1 gap-2 p-2">
+        <div className="flex items-center">
+          <SidebarTrigger />
+          <Label>Markdown Editor</Label>
+        </div>
+        <div className="flex flex-1">
+          <div className="flex flex-1 border-1 rounded-md py-4 px-2">
+            <MarkdownEditor />
+          </div>
+        </div>
       </div>
-    </div>
+    </SidebarProvider>
   );
 }
 
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -5,29 +5,12 @@ import './index.css';
 import App from './app.tsx';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import {
-  Label,
-  SidebarProvider,
-  SidebarTrigger,
-  ThemeProvider,
-} from '@repo/ui';
-import { AppSidebar } from './components/app-sidebar.tsx';
+import { ThemeProvider } from '@repo/ui';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="system">
-      <SidebarProvider>
-        <AppSidebar />
-        <div className="flex flex-col flex-1 gap-2 p-2">
-          <div className="flex items-center">
-            <SidebarTrigger />
-            <Label>Markdown Editor</Label>
-          </div>
-          <div className="flex flex-1">
-            <App />
-          </div>
-        </div>
-      </SidebarProvider>
+      <App />
     </ThemeProvider>
   </StrictMode>
 );
