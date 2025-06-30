a3e7ff3965927a8f469d13934b8a2fee1d0f7a29
nqdd <datnq1210@gmail.com>
2025-06-30 23:24:24 +0700

    refactor(router): remove unused documentation and settings pages
    Clean up routing by removing unused documentation and settings pages that are no longer needed in the application

--- app/presentation/web/src/router.tsx
+++ app/presentation/web/src/router.tsx
@@ -1,12 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { RootLayout } from './layouts/root-layout.tsx';
 import { HomePage } from './pages/home.tsx';
-import { SettingsPage } from './pages/settings.tsx';
-import { DocumentationPage } from './pages/documentation.tsx';
-import { GettingStartedPage } from './pages/documentation/getting-started.tsx';
-import { ApiReferencePage } from './pages/documentation/api.tsx';
 import { NotFoundPage } from './pages/not-found.tsx';
-import { FolderPage } from './pages/folders/[id].tsx';
+import { FolderPage } from './pages/folder.tsx';
 import { LoginPage } from './pages/login.tsx';
 import { ProtectedRoute } from './components/protected-route.tsx';
 import { OAuthCallbackPage } from './pages/oauth-callback.tsx';
@@ -14,18 +10,6 @@ import { OAuthCallbackPage } from './pages/oauth-callback.tsx';
 export const router = createBrowserRouter([
   {
     children: [
-      {
-        path: '/login',
-        element: <LoginPage />,
-      },
-      {
-        path: '/oauth/callback',
-        element: <OAuthCallbackPage />,
-      },
-      {
-        path: '*',
-        element: <NotFoundPage />,
-      },
       {
         path: '/',
         element: (
@@ -39,26 +23,22 @@ export const router = createBrowserRouter([
             element: <HomePage />,
           },
           {
-            path: 'documentation',
-            element: <DocumentationPage />,
-          },
-          {
-            path: 'documentation/getting-started',
-            element: <GettingStartedPage />,
-          },
-          {
-            path: 'documentation/api',
-            element: <ApiReferencePage />,
-          },
-          {
-            path: 'settings',
-            element: <SettingsPage />,
-          },
-          {
             path: 'folders/:id',
             element: <FolderPage />,
           },
         ],
+      },
+      {
+        path: '/login',
+        element: <LoginPage />,
+      },
+      {
+        path: '/oauth/callback',
+        element: <OAuthCallbackPage />,
+      },
+      {
+        path: '*',
+        element: <NotFoundPage />,
       },
     ],
   },
